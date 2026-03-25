7f835b20b3cf453cbab52e4c543cc6beceaee209
Pollyanny <86726960+pollybrandao@users.noreply.github.com>
2026-03-25 13:39:24 +0000

    Modifica a frase para teste de deploy na vercel

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Olá Mundo! :D</h1>
+    return <div><h1>Olá Mundo! :D Tudo certo? </h1> <h2> Lu, já te falei que os ingressos para a minha apresentação começam a ser vendidos em Abril? </h2></div>
 }
 
 export default Home;
